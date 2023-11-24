996e3e66539f6547b62d058564675b1074b3b5d3
chenjingdev <chenjingdev@gmail.com>
2023-11-25 01:14:35 +0900

    active 종목에 따른 스크롤 높이 변화

--- content.js
+++ content.js
@@ -1,20 +1,31 @@
 document.addEventListener("keydown", handleKeyDown);
-
-// 방향키에 대한 이벤트를 처리하는 함수
 function handleKeyDown(event) {
+  const parentCustomElement = document.querySelector(".scrollB > div > div");
+  const childElement = document.querySelector(".scrollB table tr.on");
+  const childElementTop = childElement.offsetTop;
+  const childElementHeight = childElement.clientHeight;
+
   switch (event.key) {
     case "ArrowUp":
       console.log("위쪽 방향키가 눌렸습니다.");
-      document.querySelector(".scrollB table tr.on").previousSibling.click();
-      // 여기에 원하는 동작을 추가할 수 있습니다.
+      parentCustomElement.scrollTo({
+        top: childElementTop - childElementHeight * 2,
+      });
+
+      childElement.previousSibling.click();
+
       break;
     case "ArrowDown":
       console.log("아래쪽 방향키가 눌렸습니다.");
-      document.querySelector(".scrollB table tr.on").nextSibling.click();
-      // 여기에 원하는 동작을 추가할 수 있습니다.
+
+      parentCustomElement.scrollTo({
+        top: childElementTop,
+      });
+
+      childElement.nextSibling.click();
+
       break;
     default:
-      // 다른 키에 대한 처리도 가능합니다.
       break;
   }
 }
