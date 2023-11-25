d4778907cbe43f1e6a5cc0132995fa381a0886a1
chenjingdev <chenjingdev@gmail.com>
2023-11-26 00:45:31 +0900

    활성화된 종목이 화면에 보이도록 스크롤 기능 추가

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
-// background.js
-chrome.runtime.onInstalled.addListener(function () {
-  console.log("Extension Installed");
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "contentScriptLoaded") {
+    // 여기에서 document ready와 같은 작업 수행
+    console.log("Content script is loaded.");
+
+    // 응답을 보내어 컨텐츠 스크립트에 알림
+    sendResponse({ message: "Document is ready in the background page." });
+  }
 });
--- content.js
+++ content.js
@@ -1,31 +1,68 @@
 document.addEventListener("keydown", handleKeyDown);
 function handleKeyDown(event) {
   const parentCustomElement = document.querySelector(".scrollB > div > div");
-  const childElement = document.querySelector(".scrollB table tr.on");
-  const childElementTop = childElement.offsetTop;
-  const childElementHeight = childElement.clientHeight;
-
+  const target = document.querySelector(".scrollB table tr.on");
+  const targetTop = target.offsetTop;
+  const targetHeight = target.clientHeight;
   switch (event.key) {
     case "ArrowUp":
-      console.log("위쪽 방향키가 눌렸습니다.");
       parentCustomElement.scrollTo({
-        top: childElementTop - childElementHeight * 2,
+        top: targetTop - targetHeight * 2,
       });
-
-      childElement.previousSibling.click();
-
+      target.previousSibling.click();
       break;
     case "ArrowDown":
-      console.log("아래쪽 방향키가 눌렸습니다.");
-
       parentCustomElement.scrollTo({
-        top: childElementTop,
+        top: targetTop,
       });
-
-      childElement.nextSibling.click();
-
+      target.nextSibling.click();
       break;
     default:
       break;
   }
 }
+
+chrome.runtime.sendMessage({ action: "contentScriptLoaded" }, function () {
+  const intervalId = setInterval(scrollToTarget, 10);
+
+  function scrollToTarget() {
+    if (!window.location.href.includes("https://upbit.com/exchange")) {
+      clearInterval(intervalId);
+      return;
+    }
+    const parentCustomElement = document.querySelector(".scrollB > div > div");
+    const target = document.querySelector(".scrollB table tr.on");
+    const textDectect = document.querySelector(".percent em")?.innerText;
+
+    if (!textDectect) return;
+    if (isScrollAtBottom()) {
+      clearInterval(intervalId);
+      return;
+    }
+
+    if (!isElementInViewport(target)) {
+      parentCustomElement?.scrollBy({
+        top: 45,
+      });
+      return;
+    } else {
+      clearInterval(intervalId);
+      return;
+    }
+  }
+
+  function isElementInViewport(target) {
+    if (!target) return false;
+    const rect = target.getBoundingClientRect();
+    if (rect.top < 270) return true;
+  }
+
+  function isScrollAtBottom() {
+    const parentCustomElement = document.querySelector(".scrollB > div > div");
+    const currentScrollPosition = parentCustomElement?.scrollTop;
+    const maxScrollPosition =
+      parentCustomElement?.scrollHeight - parentCustomElement?.clientHeight;
+
+    return currentScrollPosition === maxScrollPosition;
+  }
+});
