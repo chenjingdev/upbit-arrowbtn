a8b044f48437aedf6de55d26d5b44e3d68cebd1f
chenjingdev <chenjingdev@gmail.com>
2023-11-26 19:32:54 +0900

    아이콘 변경, 특정 주소에서만 실행되도록 변경

--- content.js
+++ content.js
@@ -1,68 +1,70 @@
-document.addEventListener("keydown", handleKeyDown);
-function handleKeyDown(event) {
-  const parentCustomElement = document.querySelector(".scrollB > div > div");
-  const target = document.querySelector(".scrollB table tr.on");
-  const targetTop = target.offsetTop;
-  const targetHeight = target.clientHeight;
-  switch (event.key) {
-    case "ArrowUp":
-      parentCustomElement.scrollTo({
-        top: targetTop - targetHeight * 2,
-      });
-      target.previousSibling.click();
-      break;
-    case "ArrowDown":
-      parentCustomElement.scrollTo({
-        top: targetTop,
-      });
-      target.nextSibling.click();
-      break;
-    default:
-      break;
-  }
-}
-
-chrome.runtime.sendMessage({ action: "contentScriptLoaded" }, function () {
-  const intervalId = setInterval(scrollToTarget, 10);
-
-  function scrollToTarget() {
-    if (!window.location.href.includes("https://upbit.com/exchange")) {
-      clearInterval(intervalId);
-      return;
-    }
+if (window.location.href.includes("https://upbit.com/exchange")) {
+  document.addEventListener("keydown", handleKeyDown);
+  function handleKeyDown(event) {
     const parentCustomElement = document.querySelector(".scrollB > div > div");
     const target = document.querySelector(".scrollB table tr.on");
-    const textDectect = document.querySelector(".percent em")?.innerText;
+    const targetTop = target.offsetTop;
+    const targetHeight = target.clientHeight;
+    switch (event.key) {
+      case "ArrowUp":
+        parentCustomElement.scrollTo({
+          top: targetTop - targetHeight * 2,
+        });
+        target.previousSibling.click();
+        break;
+      case "ArrowDown":
+        parentCustomElement.scrollTo({
+          top: targetTop,
+        });
+        target.nextSibling.click();
+        break;
+      default:
+        break;
+    }
+  }
 
-    if (!textDectect) return;
-    if (isScrollAtBottom()) {
-      clearInterval(intervalId);
-      return;
+  chrome.runtime.sendMessage({ action: "contentScriptLoaded" }, function () {
+    const intervalId = setInterval(scrollToTarget, 10);
+
+    function scrollToTarget() {
+      const parentCustomElement = document.querySelector(
+        ".scrollB > div > div"
+      );
+      const target = document.querySelector(".scrollB table tr.on");
+      const textDectect = document.querySelector(".percent em")?.innerText;
+
+      if (!textDectect) return;
+      if (isScrollAtBottom()) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      if (!isElementInViewport(target)) {
+        parentCustomElement?.scrollBy({
+          top: 45,
+        });
+        return;
+      } else {
+        clearInterval(intervalId);
+        return;
+      }
     }
 
-    if (!isElementInViewport(target)) {
-      parentCustomElement?.scrollBy({
-        top: 45,
-      });
-      return;
-    } else {
-      clearInterval(intervalId);
-      return;
+    function isElementInViewport(target) {
+      if (!target) return false;
+      const rect = target.getBoundingClientRect();
+      if (rect.top < 270) return true;
     }
-  }
 
-  function isElementInViewport(target) {
-    if (!target) return false;
-    const rect = target.getBoundingClientRect();
-    if (rect.top < 270) return true;
-  }
+    function isScrollAtBottom() {
+      const parentCustomElement = document.querySelector(
+        ".scrollB > div > div"
+      );
+      const currentScrollPosition = parentCustomElement?.scrollTop;
+      const maxScrollPosition =
+        parentCustomElement?.scrollHeight - parentCustomElement?.clientHeight;
 
-  function isScrollAtBottom() {
-    const parentCustomElement = document.querySelector(".scrollB > div > div");
-    const currentScrollPosition = parentCustomElement?.scrollTop;
-    const maxScrollPosition =
-      parentCustomElement?.scrollHeight - parentCustomElement?.clientHeight;
-
-    return currentScrollPosition === maxScrollPosition;
-  }
-});
+      return currentScrollPosition === maxScrollPosition;
+    }
+  });
+}
